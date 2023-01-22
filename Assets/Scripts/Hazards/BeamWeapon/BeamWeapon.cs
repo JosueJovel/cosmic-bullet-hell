dd3d17bd29aa4e8727c8e76f117ecfe7a7c226c6
BuildTools <unconfigured@null.spigotmc.org>
2023-01-21 19:25:52 -0600

    Reworked Beam weapon script

--- Assets/Scripts/Hazards/BeamWeapon/BeamWeapon.cs
+++ Assets/Scripts/Hazards/BeamWeapon/BeamWeapon.cs
@@ -7,52 +7,36 @@ public class BeamWeapon : MonoBehaviour
 
     public GameObject BeamBlast; //this grabs the BeamBlast prefab to instantiate with
     public GameObject AimingLaser; //this grabs the AimingLaser prefab to instantiate with
-    
     private GameObject InstantiatedLaser; //instantiated laser is stored here; need a reference to aiming laser object to check if it exists or not
 
     
     public Vector3 MuzzleLocation = new Vector3(0, -5.25f, 0); //location that AimingLaser and BeamBlast will appear from
 
-    public float AimingLaserTimer = 2.0f; //how long the the aiming laser will appear for
     public float BeamBlastTimer = 1.0f; //how long the beam blast will appear for
-    public float Interval = 5.0f; //interval between beam weapon firings
-    private float Timer = 0f; //keep track of time passed for the other variables
+    public float Interval = 1f; //interval between beam weapon firings
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(FireLaser());    //Firing the laser is handled via coroutine, because of the time delay component between telegraph and attack
 
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FireLaser() 
     {
-        Timer += Time.deltaTime; //simple timer that goes up in value over time normalized with the time between frames
-
-        if (InstantiatedLaser != null) //if aiming laser exists -> count down aiming laser timer
-        {
-            AimingLaserTimer -= Time.deltaTime;
-        }
-        
-        if (Timer >= Interval) //if timer is over interval time then execute code
-        {
-            Timer = 0f; //reset timer
-
-            //instantiates aiming laser with (aiming laser prefab, beam weapon position - certain y value so aiming laser doesn't appear in the middle of the beam weapon, rotation identity)
-            InstantiatedLaser = Instantiate(AimingLaser, transform.position + MuzzleLocation, Quaternion.identity);
-            Destroy(InstantiatedLaser, AimingLaserTimer);
-        }
-
-
-        //once aiming laser has been on for however many seconds, then BeamBlast is instantiated. Set to <= 0.01 because sometimes AimingLaserTimer ends up
-        //at a value of 0.000124 or some other random small number and doesn't execute properly
-        if (AimingLaserTimer <= 0.01) 
-        {
-            AimingLaserTimer = 2.0f;
-            //destroys and instantiates in same line; second parameter is how long the BeamBlast sticks around for
-            Destroy(Instantiate(BeamBlast, transform.position + MuzzleLocation, Quaternion.identity), (int) BeamBlastTimer); 
-        }
-
+        InstantiatedLaser = Instantiate(AimingLaser, transform.position + MuzzleLocation, Quaternion.identity);   //Instantiates the aiming laser at the location of the attached object
+        yield return new WaitForSeconds(Interval);              //Keeps aiming laser visible for (Interval) seconds
+        Destroy(InstantiatedLaser);                             //Aiming laser is destroyed  
+        BeamBlast = Instantiate(BeamBlast, transform.position + MuzzleLocation, Quaternion.identity); //Beam blast is instantiated, replacing the aiming laser
+        yield return new WaitForSeconds(BeamBlastTimer);        //Keeps beam blast on screen for (BeamBlastTimer) seconds
+        Destroy(BeamBlast);                                     //Destroy beam blast, end of firing process. 
     }
+
+    //Note: Only fires once, then never does anything again. to fire again, a new beamweapon object must be instantiated. Other scripts that will farm patterns will take care of instantiating/destorying these.
+
+
+
+    
+
 
 }
