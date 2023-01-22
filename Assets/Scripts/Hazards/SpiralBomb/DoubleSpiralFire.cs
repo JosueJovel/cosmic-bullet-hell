ae14fd426b2c0515bc35cd16af6817016f6469ca
BuildTools <unconfigured@null.spigotmc.org>
2023-01-21 18:30:26 -0600

    Created Spiral bullet bomb (+ prefab)

--- Assets/Scripts/Hazards/SpiralBomb/DoubleSpiralFire.cs
+++ Assets/Scripts/Hazards/SpiralBomb/DoubleSpiralFire.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-//NOTE: This script is attached to whatever game object is desired to be the source of this pattern. 
-public class SpreadFire : MonoBehaviour
+public class DoubleSpiralFire : MonoBehaviour
 {
-    [SerializeField] private int bulletsAmount = 10; //How many bullets to be fired per wave
-    [SerializeField] private float startAngle = 90f, endAngle = 270f;
-    [SerializeField] private float fireRate = .5f;
+
+    [SerializeField] private float angle = 0f;
+    [SerializeField] private float fireRate = .1f;
+    [SerializeField] private float angleStep = 10f;
     private Vector2 bulletMoveDirection;
 
     // Start is called before the first frame update
@@ -22,14 +21,15 @@ public class SpreadFire : MonoBehaviour
 
     //This defines the pattern that the bullets will come out with
     private void Fire() { 
-        float angleStep = (endAngle - startAngle) / bulletsAmount;  //Dictates how much space should be between each bullet to ensure even spacing
-        float angle = startAngle;   //This is the starting angle for the first bullet
-
-        for (int i = 0; i < bulletsAmount; i++)   //This for loop calculates each bullet's individual direction, to ensure a spread pattern
+   
+        
+        for (int i = 0; i <= 1; i++)   //This for loop is used to fire each bullet that will be part of one of the spiral arms. In this case, it's a double armed spiral, so for loop triggers twice for every call
         {
-            //These determine the coordinates for the endpoint of a bullet, on the unit circle, if it were fired in the current angle
-            float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);    
-            float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
+            //These determine the coordinates for the endpoint of a bullet, on the unit circle, if it were fired in the current angle.
+            //Because this for loop runs twice, two bullets are spawned: One in the desired angle, and another in the opposite direction.
+            //If more spiral arms are desired, the for loop will iterate more times (one for each desired spiral arm), and the angle increment (180f here) will need to be adjusted accordingly
+            float bulDirX = transform.position.x + Mathf.Sin((angle + 180f * i ) * Mathf.PI / 180f);   
+            float bulDirY = transform.position.y + Mathf.Cos((angle + 180f * i ) * Mathf.PI / 180f);
 
             Vector3 bulmoveVector = new Vector3(bulDirX, bulDirY, 0f);   //Direction vector is then determined from coordinates
             Vector2 bulDir = (bulmoveVector - transform.position).normalized;  //2d Direction is then extracted and normalized
@@ -41,6 +41,10 @@ public class SpreadFire : MonoBehaviour
                 bul.GetComponent<Bullet>().SetMoveDirection(bulDir);  //Bullet is fired with direction calculated above
 
             angle += angleStep;  //increments the next angle for the bullet to come out of.
+        }
+
+        if (angle >= 360f) {
+            angle = 0f;
         }
     }
 }
